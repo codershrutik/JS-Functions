8cdfd8e104dd2552c6b19989d329f97408eb4d87
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-06 09:15:54 +0000

    Functions returning functions

--- src/app.js
+++ src/app.js
@@ -47,12 +47,30 @@
 //     return x+y;
 // }
 
-function callTwice(func){
-    func();
-    func();
+// function callTwice(func){
+//     func();
+//     func();
+// }
+// function rollDie(){
+//     const roll = Math.floor(Math.random() * 6)+1;
+//     console.log(roll);
+// }
+// callTwice(rollDie);
+
+function makeMysteryFunc(){
+    const rand = Math.random();
+    if(rand>0.5){
+        return function(){
+            console.log("You won the lottery");
+        }
+    } else{
+        return function(){
+            alert("YOU HAVE A COMPUTER VIRUS");
+            alert("STOP TRYING TO CLOSE THIS WINDOW");
+            alert("STOP TRYING TO CLOSE THIS WINDOW");
+            alert("STOP TRYING TO CLOSE THIS WINDOW");
+            alert("STOP TRYING TO CLOSE THIS WINDOW");
+            alert("STOP TRYING TO CLOSE THIS WINDOW");
+        }
+    }
 }
-function rollDie(){
-    const roll = Math.floor(Math.random() * 6)+1;
-    console.log(roll);
-}
-callTwice(rollDie);
