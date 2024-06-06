727442519fb58ac78fcf55dfa6769e397b0ebc47
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-06 01:58:39 +0000

    New example added

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
-function collectEggs(){
-    let totalEggs = 6;
-    console.log(totalEggs);
-}
+// function collectEggs(){
+//     let totalEggs = 6;
+//     console.log(totalEggs);
+// }
 
-console.log(totalEggs);
-collectEggs();
-console.log(totalEggs);
+// console.log(totalEggs);
+// collectEggs();
+// console.log(totalEggs);
+
+let bird = 'Parrot';
+function birdWatch(){
+    let bird = 'Pigeon';
+    console.log(bird);
+}
