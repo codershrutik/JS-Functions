0686b3664b3632e194781595207ea747afbc82b8
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-06 09:11:45 +0000

    Higher order functions added

--- src/app.js
+++ src/app.js
@@ -43,6 +43,16 @@
 //     return x+y;
 // }
 
-const add = function(x,y){
-    return x+y;
+// const add = function(x,y){
+//     return x+y;
+// }
+
+function callTwice(func){
+    func();
+    func();
 }
+function rollDie(){
+    const roll = Math.floor(Math.random() * 6)+1;
+    console.log(roll);
+}
+callTwice(rollDie);
